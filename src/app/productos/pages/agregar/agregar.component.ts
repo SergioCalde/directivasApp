fd58cbabc5a29c8ba50c97b46cf9bc95e9ea47c7
SergioCalde <sergio.calderon.dm@outlook.com>
2022-09-03 18:44:47 -0600

    Crear formulario reactivo

---
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styles: [
+  ]
+})
+export class AgregarComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', Validators.required]
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+
+  tieneErrores( campo: string): boolean {
+    return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+}
